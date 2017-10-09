6b58b847d7bb64ffca6e3d0b46b5a24c1dd7ecdc
Chris <Chris@MaryJanes-MacBook-Pro.local>
2017-10-08 21:39:02 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var box=document.getElementById('display');
+
+function addtoscreen(x)
+{
+	box.value +=x;
+	if(x=='c')
+	{
+		box.value='';
+	}
+	
+}
+
+function answer()
+{
+	x=box.value;
+	box.value=x;
+	x=eval(x);
+	box.value=x;
+}
+
+function backspace()
+{
+	var number=box.value;
+	var len=number.length -1;
+	var newnumber=number.substring(0,len);
+	box.value=newnumber;
+}
